62222fd2cacfb41afa6e7273a62710772c6774d3
Данила Чуприн <danilacuprin@MacBook-Pro-Danila.local>
2024-09-02 10:14:44 +0300

    Fixed decorator for loading client

--- api_gateway/api/users/controllers.py
+++ api_gateway/api/users/controllers.py
@@ -6,21 +6,21 @@ class UserService(ClientGRPCLoader):
     address: str = microservices["user"]["address"]
     path_to_proto: str = microservices["user"]["path_to_proto"]
 
-    @ClientGRPCLoader.load_client
+    @ClientGRPCLoader.load_client(address, path_to_proto)
     async def create_user(self, name: str, surname: str, interface: ClientInterface):
         return await interface.client.CreateUser(
             interface.protos.CreateUserRequest(name=name, surname=surname),
             timeout=5,
         )
 
-    @ClientGRPCLoader.load_client
+    @ClientGRPCLoader.load_client(address, path_to_proto)
     async def get_users(self, interface: ClientInterface):
         return await interface.client.GetUsers(
             interface.protos.GetUsersRequest(),
             timeout=5,
         )
 
-    @ClientGRPCLoader.load_client
+    @ClientGRPCLoader.load_client(address, path_to_proto)
     async def delete_user(self, user_id: str, interface: ClientInterface):
         return await interface.client.DeleteUser(
             interface.protos.DeleteUserRequest(id=user_id),
--- api_gateway/grpc_loader/load_client_grpc.py
+++ api_gateway/grpc_loader/load_client_grpc.py
@@ -1,5 +1,4 @@
-import functools
-from typing import Any, Callable, Awaitable
+from typing import Any
 
 import grpc
 
@@ -13,19 +12,24 @@ class ClientGRPCLoader:
     address: str
     path_to_proto: str
 
-    @classmethod
-    def load_client(cls, func):
+    @staticmethod
+    def load_client(address, path_to_proto):
+        # TODO: подумать над этим
+        def load_client_inner(func):
+            async def inner(*args, **kwargs):
+                protos, services = grpc.protos_and_services(path_to_proto)  # type: ignore
+                channel = grpc.aio.insecure_channel(address)
+                stub_service_name = next(
+                    filter(lambda x: "Stub" in x, services.__dict__)
+                )
+                client = services.__dict__[stub_service_name](channel)
 
-        async def inner(*args, **kwargs):
-            protos, services = grpc.protos_and_services(cls.path_to_proto)  # type: ignore
-            channel = grpc.aio.insecure_channel(cls.address)
-            stub_service_name = next(filter(lambda x: "Stub" in x, services.__dict__))
-            client = services.__dict__[stub_service_name](channel)
+                client_and_protos_object = ClientInterface()
+                client_and_protos_object.__dict__.update(
+                    {"client": client, "protos": protos}
+                )
+                return await func(*args, **kwargs, interface=client_and_protos_object)
 
-            client_and_protos_object = ClientInterface()
-            client_and_protos_object.__dict__.update(
-                {"client": client, "protos": protos}
-            )
-            return await func(*args, **kwargs, interface=client_and_protos_object)
+            return inner
 
-        return inner
+        return load_client_inner
